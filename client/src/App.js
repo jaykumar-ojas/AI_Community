995f6a8eef26855549108172a62d51e73ccce22d
jaykumar-ojas <jaykumar.gupta@ojas-it.com>
2025-01-30 18:15:38 +0530

    fixing mess

--- client/src/App.js
+++ client/src/App.js
@@ -1,74 +1,67 @@
-import React, { useEffect, useRef, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./component/Navbar/Navbar";
 import DashBoardPage from "./pages/DashBoardPage/DashBoardPage";
 import Login from "./component/Auth/Login";
 import Register from "./component/Auth/Register";
 import UserProfile2 from "./component/UserProfile/userProfile2";
+import UserProfile from "./component/UserProfile/userProfile";
 import Error from "./component/Error/error";
 import UpdatePassword from "./component/Auth/updatePassword";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from "react";
+import Context from "./component/ContextProvider/context";
 import ForgotPassword from "./component/Auth/ForgotPassword";
 import VerfiyOtp from "./component/Auth/verifyOtp";
 import PostContent from "./component/Postcontent/postcontent";
 import PostData from "./component/Postcontent/PostData";
 
-const Layout = () => {
-  const location = useLocation();
-  const isLoginPage = location.pathname === "/login";
-  const [lastPage, setLastPage] = useState(null);
-  const contentRef = useRef(null);
-
-  useEffect(() => {
-    if (!isLoginPage && contentRef.current) {
-      // Capture the last visible page (before navigating to login)
-      setLastPage(contentRef.current.innerHTML);
-    }
-  }, [location.pathname]);
-
-  return (
-    <div className="relative h-screen w-screen overflow-hidden">
-      {/* Show the last captured page as a blurred background on login */}
-      {isLoginPage && lastPage && (
-        <div
-          className="absolute inset-0 z-0 overflow-hidden backdrop-blur-md transition-all duration-500"
-          dangerouslySetInnerHTML={{ __html: lastPage }}
-        />
-      )}
-
-      {/* Content Wrapper */}
-      <div
-        ref={contentRef}
-        className={`relative z-10 w-full h-full ${isLoginPage ? "hidden" : ""}`}
-      >
-        <Routes>
-          <Route path="/" element={<DashBoardPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/userprofile" element={<UserProfile2 />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/verify-otp/:id" element={<VerfiyOtp />} />
-          <Route path="/update-password/:id" element={<UpdatePassword />} />
-          <Route path="/userPost/:id" element={<PostContent />} />
-          <Route path="/test2" element={<PostData />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </div>
-
-      {/* Login Page Overlay */}
-      {isLoginPage && <Login />}
-    </div>
-  );
-};
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+  {
+    path: "/",
+    element: <DashBoardPage></DashBoardPage>,
+  },
+  {
+    path: "*",
+    element: <Error></Error>,
+  },
+  {
+    path: "/userprofile",
+    element: <UserProfile2></UserProfile2>,
+  },
+  {
+    path: "/forgot-password",
+    element: <ForgotPassword></ForgotPassword>,
+  },
+  {
+    path: "/verify-otp/:id",
+    element:<VerfiyOtp></VerfiyOtp>
+  },
+  {
+    path:'/update-password/:id',
+    element:<UpdatePassword></UpdatePassword>
+  },
+  {
+    path:"/userPost/:id",
+    element:<PostContent></PostContent>
+  },
+  {
+    path:'/test2',
+    element:<PostData></PostData>
+  }
+]);
 
 function App() {
   return (
-    <Router>
-      <Layout />
-    </Router>
+        <RouterProvider router={router} />
   );
 }
 
