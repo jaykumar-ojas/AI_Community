c41743b6456514b70ac114c75bf80f996d9928bd
Jaykumar gupta <guptajay201@gmail.com>
2025-03-10 20:23:19 +0530

    adjust things

--- client/src/component/Card/Card.js
+++ client/src/component/Card/Card.js
@@ -26,11 +26,32 @@ const Card = ({ post }) => {
       className="group border rounded-lg h-72 w-96 overflow-hidden relative hover:cursor-pointer"
     >
       <div className="h-full w-full">
-        <img
+        {post.fileType =="image" && <img
           src={post.signedUrl}
           className="h-full w-full object-cover opacity-95 hover:opacity-100 transition duration-300"
           alt="Card Image"
-        />
+        />}
+        {post.fileType =="video" && <video
+          src={post.signedUrl}
+          className="h-full w-full object-cover opacity-95 hover:opacity-100 transition duration-300"
+          autoPlay
+          loop
+          muted
+          playsInline
+          />}
+         {post.fileType === "audio" && (
+          <div className="h-full w-full flex items-center justify-center bg-gray-800 p-4">
+            <div className="p-4 bg-gray-900 rounded-lg shadow-lg w-80">
+              <audio
+                src={post.signedUrl}
+                className="w-full opacity-95 hover:opacity-100 transition duration-300"
+                controls
+              />
+            </div>
+          </div>
+        )}
+
+
         {/* Hidden div to show on hover */}
         <div className="absolute top-2 left-2 flex items-center gap-2 p-2 rounded-lg bg-white bg-opacity-0 opacity-0 transition duration-700 group-hover:opacity-100 group-hover:bg-opacity-50">
           <img
--- client/src/component/Navbar/Navbar.js
+++ client/src/component/Navbar/Navbar.js
@@ -61,7 +61,7 @@ import { Link } from "react-router-dom";
 
 
     return (
-      <Disclosure as="nav" className="bg-gray-100 shadow-md">
+      <Disclosure as="nav" className="sticky top-0 z-50 bg-gray-100 shadow-md">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-20">
             {/* Mobile Menu */}
